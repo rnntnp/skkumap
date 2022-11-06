ca9b293ed46f96a0cc2f845ef550bcfe85af2492
Jaein Park <jaynap@g.skku.edu>
2022-11-06 18:15:24 +0900

    /

--- app/src/main/java/com/team6/skkumap/MainActivity.java
+++ app/src/main/java/com/team6/skkumap/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("bbs");
+        myRef.child("user0").setValue("user0");
+//        myRef.child("user0").
     }
 }
